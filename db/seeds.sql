c2a3db59699e86b4d8e6dff2fec19880977e4f29
RyanMoven <ryanmoven@gmail.com>
2018-02-09 14:27:54 -0500

    _db Established

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,3 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burger (burger_name) VALUES ("The Clogger");
+INSERT INTO burger (burger_name) VALUES ("The Flatline");
+INSERT INTO burger (burger_name, devoured) VALUES ("The Itis", true);
